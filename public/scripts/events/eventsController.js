9112278d1fff8092b6ebdcd3ccd7d013fd1aa1b4
Rad0slav <radutataru@yahoo.com>
2016-06-23 23:54:11 +0300

    Update eventsController.js

--- public/scripts/events/eventsController.js
+++ public/scripts/events/eventsController.js
@@ -1,7 +1,5 @@
 module.controller('EventsController', ['$scope', '$http', '$resource', function($scope, $http, $resource){
 
-//http://localhost:3000/events?lat=40.710803&lng=-73.964040&distance=1000&sort=venue&access_token=YOUR_APP_ACCESS_TOKEN
-//http://localhost:3000/events?lat=40.710803&lng=-73.964040&distance=1000&sort=venue&access_token=1009698882417660%7CyKAzMQdcUUvH7h9vflg6hCxDEEg
    var Events = $resource('/events?lat=:lat&lng=:lng&distance=:distance&sort=:sort&access_token=:access_token', 
         {
             lat: '@lat',
@@ -17,7 +15,7 @@ module.controller('EventsController', ['$scope', '$http', '$resource', function(
             lng: '26.102170',
             distance: '1000',
             sort: 'venue',
-            access_token: '1009698882417660|yKAzMQdcUUvH7h9vflg6hCxDEEg'
+            access_token: 'fff'
         })
         .$promise.then(function(events){
             console.log(events.events);
